035e24faf8f2d14016b92637cdd8aa2d3423442b
Anthony Laplane <anthony.laplane@gmail.com>
2025-01-29 14:11:12 +0100

    fixing path

--- templates/footer.php
+++ templates/footer.php
@@ -4,8 +4,8 @@
 
 <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Accueil</a></li>
-        <li class="nav-item"><a href="index.php?controller=book&action=list" class="nav-link px-2">Liste des livres</a></li>
+        <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Home</a></li>
+        <li class="nav-item"><a href="index.php?controller=book&action=list" class="nav-link px-2">Books list</a></li>
     </ul>
     <p class="text-center text-muted">© <?= date('Y'); ?> - Bookeo</p>
 </footer>
